58a1ee2ad4071d74dc7e6030948be22604b086fe
SgtMarmite <d.novotny31415@gmail.com>
2024-01-17 14:15:04 +0100

    list only datetime fields in sync action

--- src/component.py
+++ src/component.py
@@ -3,6 +3,8 @@ Zoho CRM Extractor component main module.
 
 """
 import logging
+import dateparser
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import List, Optional
 import os
@@ -30,11 +32,12 @@ KEY_MODULE_RECORDS_DOWNLOAD_CONFIG = "module_records_download_config"
 KEY_OUTPUT_TABLE_NAME = "output_table_name"
 KEY_MODULE_NAME = "module_name"
 KEY_FIELD_NAMES = "field_names"
+KEY_GROUP_SYNC_OPTIONS = "sync_options"
+KEY_SYNC_MODE = "sync_mode"
 KEY_FILTERING_CRITERIA = "filtering_criteria"
 
-REQUIRED_PARAMETERS = [
-    KEY_MODULE_RECORDS_DOWNLOAD_CONFIG,
-]
+
+REQUIRED_PARAMETERS = [KEY_MODULE_RECORDS_DOWNLOAD_CONFIG]
 
 # Other constants
 REGION_CODE = "EU"
@@ -67,26 +70,24 @@ class ZohoCRMExtractor(ComponentBase):
         """
         module_name: str = config[KEY_MODULE_NAME]
         field_names: Optional[List[str]] = config.get(KEY_FIELD_NAMES)
-        filtering_criteria_dict: Optional[dict] = config.get(KEY_FILTERING_CRITERIA)
-
-        self.validate_filtering_criteria(filtering_criteria_dict)
 
         filtering_criteria = None
-        if filtering_criteria_dict:
-            key_comparator = filtering_criteria_dict.get(zoho.bulk_read.KEY_COMPARATOR)
-            key_group = filtering_criteria_dict.get(zoho.bulk_read.KEY_GROUP)
+        if self.filtering_criteria_dict:
+            key_comparator = self.filtering_criteria_dict.get(zoho.bulk_read.KEY_COMPARATOR)
+            key_group = self.filtering_criteria_dict.get(zoho.bulk_read.KEY_GROUP)
 
             if key_comparator:
-                filtering_criteria = zoho.bulk_read.BulkReadJobFilteringCriterion.from_dict(filtering_criteria_dict)
+                filtering_criteria = zoho.bulk_read.BulkReadJobFilteringCriterion.from_dict(
+                    self.filtering_criteria_dict)
             elif key_group:
-                filtering_criteria = zoho.bulk_read.BulkReadJobFilteringCriteriaGroup.from_dict(filtering_criteria_dict)
+                filtering_criteria = zoho.bulk_read.BulkReadJobFilteringCriteriaGroup.from_dict(
+                    self.filtering_criteria_dict)
 
         table_def = self.create_out_table_definition(
             name=f"{self.output_table_name}.csv",
             incremental=self.incremental,
             primary_key=[ID_COLUMN_NAME],
-            is_sliced=True,
-        )
+            is_sliced=True)
 
         os.makedirs(table_def.full_path, exist_ok=True)
         logging.info(f"Attempting to download data for output table {self.output_table_name}.")
@@ -108,7 +109,7 @@ class ZohoCRMExtractor(ComponentBase):
         self.write_manifest(table_def)
 
     @staticmethod
-    def validate_filtering_criteria(criteria: dict):
+    def validate_filtering_criteria(criteria: dict) -> None:
         # TODO: implement proper validation
         allowed_keys = ["group", "field_name", "comparator", "value", "group_operator"]
         for key in criteria:
@@ -116,7 +117,7 @@ class ZohoCRMExtractor(ComponentBase):
                 raise UserException(f"{key} is not a valid filter key.")
 
     @staticmethod
-    def get_fields(module_api_name: str) -> list:
+    def get_fields(module_api_name: str, datetype: str = None) -> list:
         fields_operations = FieldsOperations(module_api_name)
         param_instance = ParameterMap()
 
@@ -127,7 +128,8 @@ class ZohoCRMExtractor(ComponentBase):
 
             module_names = []
             for module in data._ResponseWrapper__fields:
-                module_names.append(module._Field__api_name)
+                if (not datetype) or (datetype and datetype == module._Field__data_type):
+                    module_names.append(module._Field__api_name)
         else:
             raise UserException("Cannot fetch the list of available Fields.")
 
@@ -186,6 +188,9 @@ class ZohoCRMExtractor(ComponentBase):
         if not self.user_email:
             raise UserException("Parameter user_email is mandatory.")
 
+        sync_options = params.get(KEY_GROUP_SYNC_OPTIONS)
+        self.filtering_criteria_dict = self._set_filters(sync_options)
+
         load_mode: str = params.get(KEY_GROUP_DESTINATION, {}).get(KEY_LOAD_MODE, "full_load")
         self.incremental: bool = load_mode == "incremental"
 
@@ -195,6 +200,48 @@ class ZohoCRMExtractor(ComponentBase):
         data_dir_path = Path(self.data_folder_path)
         self.tmp_dir_path = data_dir_path / TMP_DATA_DIR_NAME
         self.tmp_dir_path.mkdir(parents=True, exist_ok=True)
+
+    def _set_filters(self, sync_options: dict) -> dict:
+        sync_mode = sync_options.get(KEY_SYNC_MODE)
+
+        if sync_mode == "full_sync":
+            filtering_criteria_dict = None
+        elif sync_mode == "advanced":
+            filtering_criteria_dict = sync_options.get(KEY_FILTERING_CRITERIA)
+        elif sync_mode == "incremental_sync":
+            filtering_criteria_dict = self._get_incremental_sync_filter(sync_options)
+        else:
+            raise UserException(f"Unsupported sync_mode: {sync_mode}")
+
+        self.validate_filtering_criteria(filtering_criteria_dict)
+        return filtering_criteria_dict
+
+    def _get_incremental_sync_filter(self, sync_options: dict) -> dict:
+        value = dateparser.parse(sync_options.get("value"))
+        formatted_date = self._format_datetime_with_offset(value)
+
+        return {
+            "field_name": sync_options.get("incremental_field"),
+            "comparator": sync_options.get("operator"),
+            "value": formatted_date
+        }
+
+    @staticmethod
+    def _format_datetime_with_offset(dt) -> str:
+        date_str = dt.strftime("%Y-%m-%dT%H:%M:%S")
+        timezone_offset = datetime.now(timezone.utc).astimezone().strftime('%z')
+        return f"{date_str}{timezone_offset}"
+
+    def _list_fields(self, datetype: str = None) -> List[SelectElement]:
+        self._init_params()
+        self._init_client()
+
+        module_name = self.module_records_download_config[KEY_MODULE_NAME]
+        if not module_name:
+            raise UserException("To list available fields, module_name parameter must be set.")
+
+        modules = self.get_fields(module_name, datetype=datetype)
+        return [SelectElement(label=module, value=module) for module in modules]
 
     @sync_action("listModules")
     def list_modules(self) -> List[SelectElement]:
@@ -206,15 +253,11 @@ class ZohoCRMExtractor(ComponentBase):
 
     @sync_action("listFields")
     def list_fields(self) -> List[SelectElement]:
-        self._init_params()
-        self._init_client()
+        return self._list_fields()
 
-        module_name = self.module_records_download_config[KEY_MODULE_NAME]
-        if not module_name:
-            raise UserException("To list available fields, module_name parameter must be set.")
-
-        modules = self.get_fields(module_name)
-        return [SelectElement(label=module, value=module) for module in modules]
+    @sync_action("listFieldsDatetime")
+    def list_fields_datetime(self) -> List[SelectElement]:
+        return self._list_fields("datetime")
 
 
 """

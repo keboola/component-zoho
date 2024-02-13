2ef414924b4bda16c89dce0a16b2f5543c23b312
SgtMarmite <d.novotny31415@gmail.com>
2024-02-13 12:16:07 +0100

    remove ui options from portal properties script

--- scripts/update_dev_portal_properties.sh
+++ scripts/update_dev_portal_properties.sh
@@ -109,17 +109,3 @@ then
 else
     echo "loggerConfiguration is empty!"
 fi
-
-echo "Updating UI helpers to use"
-value=`cat component_config/uiOptions.json`
-echo "$value"
-if [ ! -z "$value" ]
-then
-    docker run --rm \
-            -e KBC_DEVELOPERPORTAL_USERNAME \
-            -e KBC_DEVELOPERPORTAL_PASSWORD \
-            quay.io/keboola/developer-portal-cli-v2:latest \
-            update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} uiOptions --value="$value"
-else
-    echo "uiOptions is empty!"
-fi
